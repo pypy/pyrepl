b6bc398ce54a9e3d62362d925a10f1e439fede24
Michael Hudson <micahel@gmail.com>
2002-10-29 14:24:36 +0000

    remove unecessary import.

--- pyrepl/fancy_termios.py
+++ pyrepl/fancy_termios.py
@@ -17,7 +17,7 @@
 # CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
 # CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-import termios, sys
+import termios
 
 class TermState:
     def __init__(self, tuples):
