591912b36ff18eca7895e002925395997a7c26da
Armin Rigo <arigo@tunes.org>
2008-04-08 10:23:09 +0000

    Add these modules directly into pyrepl, where they can be reused to give the same CPython-like-but-multiline prompt on top of CPython.

---
+++ pyrepl/simple_interact.py
@@ -0,0 +1,42 @@
+"""This is an alternative to python_reader which tries to emulate
+the CPython prompt as closely as possible, with the exception of
+allowing multiline input and multiline history entries.
+"""
+
+import sys
+from pyrepl.readline import multiline_input
+
+
+def run_multiline_interactive_console(mainmodule):
+    import code
+    if mainmodule is None:
+        import __main__ as mainmodule
+    console = code.InteractiveConsole(mainmodule.__dict__)
+
+    def more_lines(unicodetext):
+        # ooh, look at the hack:
+        src = "#coding:utf-8\n"+unicodetext.encode('utf-8')
+        try:
+            code = console.compile(src, '<input>', 'single')
+        except (OverflowError, SyntaxError, ValueError):
+            return False
+        else:
+            return code is None
+
+    while 1:
+        try:
+            ps1 = getattr(sys, 'ps1', '>>> ')
+            ps2 = getattr(sys, 'ps2', '... ')
+            try:
+                statement = multiline_input(more_lines, ps1, ps2)
+            except EOFError:
+                break
+            # XXX with Alt-Enter we can actually enter more than one
+            # statement, and compile() ignores everything after the
+            # first statement in 'single' mode...  We should either
+            # find some obscure workaround or tweak PyPy's compiler.
+            more = console.push(statement)
+            assert not more
+        except KeyboardInterrupt:
+            console.write("\nKeyboardInterrupt\n")
+            console.resetbuffer()
