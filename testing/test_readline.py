91009e4c80f3a855157fc8dc73e50ad3d7756e3c
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2013-01-18 09:38:39 +0100

    support filenos as args for readline wrapper

--- testing/test_readline.py
+++ testing/test_readline.py
@@ -4,11 +4,12 @@ import pty
 
 
 def test_raw_input():
-    readline_wrapper = _ReadlineWrapper()
     master, slave = pty.openpty()
-    readline_wrapper.f_in = slave
+    readline_wrapper = _ReadlineWrapper(slave, slave)
     os.write(master, b'input\n')
-    result = readline_wrapper.raw_input('prompt:')
+
+    result = readline_wrapper.get_reader().readline()
+    #result = readline_wrapper.raw_input('prompt:')
     assert result == 'input'
     # A bytes string on python2, a unicode string on python3.
     assert isinstance(result, str)
