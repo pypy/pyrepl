07a784d47950d70c2d0dc0e77d53a3263c78a4ec
Daniel Hahler <git@thequod.de>
2019-03-11 01:54:26 +0100

    remove testing/conftest.py: pytest_ignore_collect should not be needed

--- testing/conftest.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-def pytest_ignore_collect(path):
-    if '__pypy__' not in sys.builtin_module_names:
-        try:
-            import pyrepl
-        except ImportError:
-            return True
