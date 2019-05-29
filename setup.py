f0b4b7857b048efb7a265b15b9e156bd9e7ef61f
Daniel Hahler <git@thequod.de>
2019-05-29 17:32:21 +0200

    setup.py: use versioning=dev (#16)

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ command line interfaces, featuring:
 setup(
     name="pyrepl",
     setup_requires="setupmeta",
-    versioning="devcommit",
+    versioning="dev",
     author="Michael Hudson-Doyle",
     author_email="micahel@gmail.com",
     maintainer="Daniel Hahler",
